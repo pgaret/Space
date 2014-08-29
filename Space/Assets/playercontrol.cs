d562a8056ae75fbcfe93a0c53cad94c69244907a
chuc3 <chuc3@rpi.edu>
2014-08-28 22:01:35 -0400

    bunch of stuff. movement of thing on side of triangle

---
+++ Space/Assets/playercontrol.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playercontrol : MonoBehaviour {
+
+	public Transform next;
+	public Transform last;
+	public float speed;
+	public float slow;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {	
+			float step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards (transform.position, next.position, step);
+		} else 
+		{
+			float step = slow * Time.deltaTime;
+			transform.position = Vector3.MoveTowards (transform.position,last.position,step);
+		}
+	}
+}
