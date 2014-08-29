1fb713bbc378d9d9889a3821451893f5b4d8f819
pgaret <peregringaret@gmail.com>
2014-08-29 01:14:33 -0400

    Title screen and button madness

---
+++ Space/Assets/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	
+	public Transform title;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		SpriteRenderer sr = title.GetComponent<SpriteRenderer>();
+		
+		title.localScale = new Vector2(1, 1);
+		
+		float width = title.renderer.bounds.size.x;
+		float height = title.renderer.bounds.size.y;
+		
+		float worldscreenHeight = Camera.main.orthographicSize * 2.0f;
+		float worldscreenWidth = worldscreenHeight / Screen.height * Screen.width;
+		
+		title.localScale = new Vector2(worldscreenWidth / width, worldscreenHeight / height);
+		
+		Instantiate(title);
+	}
+	
+	void OnGUI()
+	{
+		if (GUI.Button (new Rect(Screen.width / 3, Screen.height * 3 / 4, Screen.width / 3, Screen.height / 10), "Play")) Application.LoadLevel(1);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
--- Space/Assets/Play.cs
+++ Space/Assets/Play.cs
@@ -4,20 +4,58 @@ using System.Collections;
 public class Play : MonoBehaviour {
 
 	public Transform triangle;
+	public GUIStyle fire;
+	public GUIStyle water;
+	public GUIStyle earth;
 
-	bool increaseScale = false;
+	float waterScale = 0;
+	float earthScale = 0;
+	float fireScale = 0;
+
 	float scale = .1F;
+	string text = "None";
+	
+	int earthInt = 0;
+	int waterInt = 0;
+	int fireInt = 0;
+	
+	bool earthBool = false;
+	bool waterBool = false;
+	bool fireBool = false;
 
 	// Use this for initialization
 	void Start ()
 	{
-		Screen.orientation = ScreenOrientation.Landscape;
+		Screen.orientation = ScreenOrientation.Portrait;
 	}
 	
 	void OnGUI()
 	{
-		if (GUI.Button(new Rect(Screen.width / 4, Screen.height * 9 / 10, Screen.width / 2, Screen.height / 10), "Test")) increaseScale = true;
-		else increaseScale = false;
+		if (GUI.RepeatButton(new Rect(-Screen.width / 70, Screen.height * 3.5f / 5, Screen.width * 18.3f / 50, Screen.height * 9 / 20), " ", earth))
+		{
+			text = "Earth";
+			earthInt += 1;
+			earthBool = true;
+		}
+		else earthBool = false;
+		if (GUI.RepeatButton(new Rect(Screen.width * 3.27f / 10, Screen.height * 3.5f / 5, Screen.width * 18.3f / 50, Screen.height * 9 / 20), " ", fire))
+		{
+			text = "Fire";
+			fireInt += 1;
+			fireBool = true;
+		}
+		else fireBool = false;
+		if (GUI.RepeatButton(new Rect(Screen.width * 2f / 3, Screen.height * 3.5f / 5, Screen.width * 18.3f / 50, Screen.height * 9 / 20), " ", water))
+		{
+			text = "Water";
+			waterInt += 1;
+			waterBool = true;
+		}
+		else waterBool = false;
+	
+		GUI.skin.box.alignment = TextAnchor.MiddleCenter;
+		GUI.skin.box.fontSize = 48;
+		GUI.Box (new Rect(Screen.width / 4, Screen.height / 4, Screen.width / 2, Screen.height / 2), earthInt.ToString()+" "+fireInt.ToString()+" "+waterInt.ToString());
 	}
 	
 	
@@ -25,6 +63,11 @@ public class Play : MonoBehaviour {
 	void Update ()
 	{
 //		triangle.transform.Rotate(Vector3.forward * Time.deltaTime * 10);
-		if (increaseScale == true) triangle.transform.localScale += new Vector3(.1F, .1F, 0);
+		
+//		if (waterScale == 5) triangle.transform.localScale += new Vector3(.1F, .1F, 0);
+		
+		if (earthBool == false && earthInt > 0) earthInt -= 1;
+		if (fireBool == false && fireInt > 0) fireInt -= 1;
+		if (waterBool == false && waterInt > 0) waterInt -= 1;
 	}
 }
