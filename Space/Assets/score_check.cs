0f88708c3b1503d6db9c3f683a850db41ef8565b
chuc3 <chuc3@rpi.edu>
2014-09-01 16:21:20 -0400

    some collion detection work

---
+++ Space/Assets/score_check.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class score_check : MonoBehaviour {
+
+	public Transform bar_blue;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 asdf = new Vector2 (bar_blue.position.x, bar_blue.position.y);
+		if (Physics2D.OverlapPoint (asdf))
+						print (";masdf");
+	}
+
+
+}
