4363e0418b2c83ae977271da85e3f44b6597e4a5
chuc3 <chuc3@rpi.edu>
2014-09-01 19:06:25 -0400

    idek

--- Space/Assets/score_check.cs
+++ Space/Assets/score_check.cs
@@ -13,8 +13,19 @@ public class score_check : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector2 asdf = new Vector2 (bar_blue.position.x, bar_blue.position.y);
-		if (Physics2D.OverlapPoint (asdf))
-						print (";masdf");
+		if (Physics2D.OverlapPoint (asdf)) 
+		{
+			Transform x = bar_blue.GetChild(0);
+			x.particleSystem.enableEmission=true;
+			print (";masdf");
+
+		} 
+		else 
+		{
+			Transform x = bar_blue.GetChild(0);
+			x.particleSystem.enableEmission=false;
+		}
+
 	}
 
 
