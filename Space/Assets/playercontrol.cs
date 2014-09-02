485f99c46653cf35d5be5c6fccd4fc31426b77f3
chuc3 <chuc3@rpi.edu>
2014-09-02 14:08:46 -0400

    changed particle effect. script overhaul

--- Space/Assets/playercontrol.cs
+++ Space/Assets/playercontrol.cs
@@ -3,11 +3,31 @@ using System.Collections;
 
 public class playercontrol : MonoBehaviour {
 
-	public Transform next;
-	public Transform last;
+
+	// C: change all the publics to global
+	// and set them in Start
+
+	// each of the bars we want to move
+	public Transform blue;
+	public Transform green;
+	public Transform orange;
+
+	// start and end points of bars
+	public Transform blue_next;
+	public Transform blue_last;
+	public Transform green_next;
+	public Transform green_last;
+	public Transform orange_next;
+	public Transform orange_last;
+
+
+	// speeds and speed decay
 	public float speed;
 	public float slow;
+
+	// not quite sure what this is
 	public bool activate = false;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -15,13 +35,39 @@ public class playercontrol : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (activate == true || Input.GetKey(KeyCode.Space)) {	
-			float step = speed * Time.deltaTime;
-			transform.position = Vector3.MoveTowards (transform.position, next.position, step);
-		} else 
-		{
-			float step = slow * Time.deltaTime;
-			transform.position = Vector3.MoveTowards (transform.position,last.position,step);
+		// old code
+		/*
+				if (activate == true || Input.GetKey (KeyCode.Space)) {	
+						float step = speed * Time.deltaTime;
+						transform.position = Vector3.MoveTowards (transform.position, next.position, step);
+				} else {
+						float step = slow * Time.deltaTime;
+						transform.position = Vector3.MoveTowards (transform.position, last.position, step);
+				}
+		*/
+		float step_up = speed * Time.deltaTime;
+		float step_down = slow * Time.deltaTime;
+
+
+		if (Input.GetKey (KeyCode.Z)) { print ("z");
+			blue.position = Vector3.MoveTowards (blue.position, blue_next.position, step_up);
+		} 
+		else {
+			blue.position = Vector3.MoveTowards (blue.position, blue_last.position, step_down);
+		}
+
+		if (Input.GetKey (KeyCode.X)) {
+				
+		} 
+		else {
+
+		}
+
+		if (Input.GetKey (KeyCode.C)) {
+				
+		} 
+		else {
+		
 		}
 	}
 }
--- Space/Assets/score_check.cs
+++ Space/Assets/score_check.cs
@@ -3,11 +3,17 @@ using System.Collections;
 
 public class score_check : MonoBehaviour {
 
+	// whoops have to go back and fix this function
+	// so that it is more compatable
+
 	public Transform bar_blue;
+	Transform one;
+	Transform two;
 
 	// Use this for initialization
 	void Start () {
-	
+		one = bar_blue.GetChild(0);
+		two = bar_blue.GetChild(1);
 	}
 	
 	// Update is called once per frame
@@ -15,15 +21,15 @@ public class score_check : MonoBehaviour {
 		Vector2 asdf = new Vector2 (bar_blue.position.x, bar_blue.position.y);
 		if (Physics2D.OverlapPoint (asdf)) 
 		{
-			Transform x = bar_blue.GetChild(0);
-			x.particleSystem.enableEmission=true;
-			print (";masdf");
+			one.particleSystem.enableEmission=true;
+			two.particleSystem.enableEmission=true;
+			print ("collision");
 
 		} 
 		else 
 		{
-			Transform x = bar_blue.GetChild(0);
-			x.particleSystem.enableEmission=false;
+			one.particleSystem.enableEmission=false;
+			two.particleSystem.enableEmission=false;
 		}
 
 	}
